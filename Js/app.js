d5ec9a47a4f15f382dd42f8aee1a8cc206b52c04
unknown <divyakrish568@gmail.com>
2024-07-13 00:16:54 +0530

    Portfolio version updated

---
+++ Js/app.js
@@ -0,0 +1,28 @@
+// const observer  =  new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         } else{
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+// // <script src="https://use.typekit.net/bkt6ydm.js">
+// //</script>
+
+// <script>try{Typekit.load({ async: true });}catch(e){}</script>
+// // window.onscroll = function() {myFunction()};
+
+// // function myFunction() {
+// //   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+// //   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// //   var scrolled = (winScroll / height) * 100;
+// //   document.getElementById("myBar").style.width = scrolled + "%";
+// // }
+// </script>
